ec3ae1d7496ea550575e35b73fb5e9b036d68768
avarduhi <73523717+avarduhi@users.noreply.github.com>
2020-11-18 23:33:26 +0400

    Update ReverseArray.java

--- ReverseArray.java
+++ ReverseArray.java
@@ -1,5 +1,32 @@
 package com.company;
 //Առաջադրանք 3: իմպլանտացնել reverse(int a[], int n) մեթոդը։(n-ը զանգվածի երկարությունն է)
+//Փոփոխած տարբերակ
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int[] arr = {2, 3, 4, 5, 6, 7};
+        int[] arr1 = reverse(arr, arr.length);
+        for(int value: arr1){
+            System.out.print(value + " ");
+        }
+
+    }
+
+    static int[] reverse(int a[], int n) {
+        int[] newA= new int[n];
+        for (int i=0; i<n; i++) {
+            newA[i] = a[n-i-1];
+        }
+        return newA;
+    }
+}
+
+
+
+
+
+/*
 public class Main {
 
     public static void main(String[] args) {
@@ -15,3 +42,4 @@ public class Main {
         }
     }
 }
+*/
